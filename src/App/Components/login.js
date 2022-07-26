f93e6418db419c5a69426b7ad1d4bf52418c3ec9
Shiro-cha <noum.rzdr@gmail.com>
2022-07-26 21:37:14 +0300

    message

--- src/App/Components/login.js
+++ src/App/Components/login.js
@@ -129,7 +129,7 @@ export default function Login(){
 		</DialogContent>
 		<DialogActions>
 		<Button onClick={()=>{setOpenDialog(false)}} >Annulé</Button>
-		<Button disabled={errorpassword} onClick={checkHostInfo}>Valider</Button>
+		<Button disabled={errorpassword} onClick={checkHostInfo}>Valider</Button> 
 		</DialogActions>
 		</Dialog>
 		

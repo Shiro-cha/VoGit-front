da80a81f17a03164517aa75246a24962ed401c1d
Shiro-cha <noum.rzdr@gmail.com>
2022-07-27 00:32:22 +0300

    message

--- src/App/Components/explorer.js
+++ src/App/Components/explorer.js
@@ -33,7 +33,7 @@ export default function Explorer() {
 		<Toolbar variant="dense">
 		
 		<Typography variant="h6" color="inherit" component="div">
-		Photos fsdqfdqsfdqsfdsfds
+		VoGit
 		</Typography>
 		</Toolbar>
 		</AppBar>

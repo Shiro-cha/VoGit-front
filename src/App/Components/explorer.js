39d41de500ab0057da65c5222cc751cfaa7a1733
Shiro-cha <noum.rzdr@gmail.com>
2022-07-26 22:56:35 +0300

    message

--- src/App/Components/explorer.js
+++ src/App/Components/explorer.js
@@ -20,7 +20,7 @@ export default function Explorer() {
 	return (
 		<div style={{position:"fixed",top:0,bottom:0,left:0,right:0}}>
 		<Grid container sx={{height:"100%"}}>
-		<Grid item xs={4} sx={{height:"100%"}}>
+		<Grid item xs={4} sx={{height:"100%",background:"red"}}>
 		 
 		 Vertical navbar here
 		

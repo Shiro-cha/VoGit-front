086443b966803f59411b578bf88405d74213d67e
Shiro-cha <noum.rzdr@gmail.com>
2022-07-28 22:12:53 +0300

    message

--- src/App/Components/explorer.js
+++ src/App/Components/explorer.js
@@ -77,7 +77,7 @@ export default function Explorer() {
 		
 		
 		
-		<Box sx={{ height:"100%", transform: 'translateZ(0px)', flexGrow: 1 }}>
+		<Box sx={{ height:"100%", transform: 'translateZ(0px)', flexGrow: 1 ,justifyContent:"center"}}>
 		
 		{
 			//page component

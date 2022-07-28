5e1e37552f654a9dac859e69267aaa9e615c3549
Shiro-cha <noum.rzdr@gmail.com>
2022-07-28 22:29:10 +0300

    message

--- src/App/Components/components/FileContainer.js
+++ src/App/Components/components/FileContainer.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Paper from "@mui/material/Paper"
-
+import Chip from "@mui/material/Chip"
 
 export default function FileContainer(){
 	
 	return(
 		<Paper sx={{width:"97%",height:"100%",margin:"0 auto"}} elevation={1}>
-		
+		<Chip label="Home"/>
+		<Chip label="Desktop"/>
+		<Chip label="Memoire"/>
 		</Paper>
 	)
 	

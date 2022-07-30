e85c26e9511d9ca79a7ff1abc4354aa3c97d09bb
Shiro-cha <noum.rzdr@gmail.com>
2022-07-30 23:23:53 +0300

    message

--- src/App/Components/components/HistoryContainer.js
+++ src/App/Components/components/HistoryContainer.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Paper from "@mui/material/Paper";
-
+import ListItemButton from "@mui/material/ListItemButton";
 
 export default function HistoryContainer(){
 	
 	return(
 		<Paper sx={{width:"97%",height:"100%",margin:"0 auto"}} elevation={1}>
-		history container here
+		<Container sx={{transform:"translate(0%,-50%) !important"}}>
+		<Chip label="Manage your history and file version here" sx={{cursor:"pointer"}}/>
+		</Container>
 		</Paper>
 	)
 	

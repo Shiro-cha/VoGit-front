ab63270b5bed6a1023de03a649e11f8f0f25b683
Shiro-cha <noum.rzdr@gmail.com>
2022-07-30 20:43:53 +0300

    message

--- src/App/Components/components/FileContainer.js
+++ src/App/Components/components/FileContainer.js
@@ -16,7 +16,8 @@ export default function FileContainer(){
 		<Chip label="Memoire" sx={{cursor:"pointer"}}/>
 		</Container>
 		<Box
-		sx={{display:"flex",flexDirection:"row",flexWarp:"warp !important",alignItems:"flex-start",marginTop:"20px",width:"100%",height:"100%"}}>
+		display="flex" flexFlow="row warp" alignItems="flex-start"
+		sx={{marginTop:"20px",width:"100%",height:"100%"}}>
 		<IconButton sx={{display:"flex",flexDirection:"column"}}>
 		<Folder sx={{fontSize:"70px"}}/>
 		<i className="file-name">My folder</i>

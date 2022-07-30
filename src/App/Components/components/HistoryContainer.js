bd686ff56390eed97322890f2e8d60cd9f4b852c
Shiro-cha <noum.rzdr@gmail.com>
2022-07-30 22:57:59 +0300

    message

--- src/App/Components/components/HistoryContainer.js
+++ src/App/Components/components/HistoryContainer.js
@@ -1,11 +1,13 @@
 import React from "react"
+import Paper from "@mui/material/Paper";
+
 
 export default function HistoryContainer(){
 	
 	return(
-		<div>
+		<Paper sx={{width:"97%",height:"100%",margin:"0 auto"}} elevation={1}>
 		history container here
-		</div>
+		</Paper>
 	)
 	
 }

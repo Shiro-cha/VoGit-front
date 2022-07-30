111ba8ac87f1f9bfdd36ede3fa705acd1e64e9c6
Shiro-cha <noum.rzdr@gmail.com>
2022-07-30 20:45:02 +0300

    message

--- src/App/Components/components/FileContainer.js
+++ src/App/Components/components/FileContainer.js
@@ -7,7 +7,7 @@ import IconButton from "@mui/material/IconButton"
 import Folder from '@mui/icons-material/Folder'
 
 export default function FileContainer(){
-	
+	 
 	return(
 		<Paper sx={{width:"97%",height:"100%",margin:"0 auto"}} elevation={1}>
 		<Container>

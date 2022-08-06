a9c0f35ab4a52cd8a2d079847dd0ee9763683532
Shiro-cha <noum.rzdr@gmail.com>
2022-08-06 22:50:26 +0300

    message

--- src/App/Components/components/HistoryContainer.js
+++ src/App/Components/components/HistoryContainer.js
@@ -74,7 +74,7 @@ export default function HistoryContainer(){
 			secondaryAction={
 				<>
 				<IconButton edge="end" aria-label="action"
-				aria-controls={opeMenun ? 'demo-positioned-menu' : undefined}
+				aria-controls={opeMenu ? 'demo-positioned-menu' : undefined}
 				aria-haspopup="true"
 				aria-expanded={open ? 'true' : undefined}
 				onClick={()=>{setOpenMenu(!openMenu)}}>

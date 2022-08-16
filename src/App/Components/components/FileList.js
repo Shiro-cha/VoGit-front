db44d7791189d86940eeb86cce17cd005acde23f
Shiro-cha <noum.rzdr@gmail.com>
2022-08-16 23:12:47 +0300

    message

--- src/App/Components/components/FileList.js
+++ src/App/Components/components/FileList.js
@@ -38,7 +38,7 @@ export default function FileList({folder}){
 	if(files.length===0){
 		console.log("file is null")
 		return (
-			<div style={{width:"100% !important",display:"flex",flexDirection:"column",justifyContent:"center"}}>
+			<div style={{width:"100% !important",display:"flex !important",flexDirection:"column",justifyContent:"center"}}>
 			<Skeleton width="90%" height={50} animation="wave"/>
 			<Skeleton width="90%" height={50} animation="wave"/>
 			<CircularProgress color="inherit" sx={{opacity:0.2}}/>

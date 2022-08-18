8fa2020e75a3c90d95021355a895622f02ff5f6d
Shiro-cha <noum.rzdr@gmail.com>
2022-08-18 10:50:20 +0300

    message

--- src/App/Components/components/MyHead.js
+++ src/App/Components/components/MyHead.js
@@ -2,14 +2,15 @@ import Chip from "@mui/material/Chip"
 import React from "react"
 import Skeleton from "@mui/material/Skeleton"
 
-function MyHeadEl({myHead}){
+function MyHeadEl({myHead,setCurrentFolder,sep}){
 	if(myHead.length > 0){
-		
+		let headPath = ""
  			return(
 			myHead.map(function(value){
+				headPath = headPath +sep +value
 				if(value){
 					return(
-						<Chip label={value} sx={{cursor:"pointer"}}/>
+						<Chip label={value} sx={{cursor:"pointer"}} onClick={()=>{setCurrentFolder(headPath)}}/>
 					)	
 				}
 				

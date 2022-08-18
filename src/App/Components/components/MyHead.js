ce4282ae119bce38cef08510f24f5b4188915b21
Shiro-cha <noum.rzdr@gmail.com>
2022-08-18 11:11:44 +0300

    message

--- src/App/Components/components/MyHead.js
+++ src/App/Components/components/MyHead.js
@@ -9,10 +9,9 @@ function MyHeadEl({myHead,setCurrentFolder,sep}){
 			
 			myHead.map(function(value){
 				if(value){
-					console.log(headPath)
 					headPath = headPath+sep+value
 					return(
-						<Chip label={value} sx={{cursor:"pointer"}} onClick={()=>{setCurrentFolder(headPath);console.log(headPath)}}/>
+						<Chip label={value} sx={{cursor:"pointer"}} onClick={console.log(headPath)}/>
 					)	
 				}
 				

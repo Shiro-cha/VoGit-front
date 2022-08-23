67312c8d3c48f6e9d54ed632e09dc9ab923c4b98
Shiro-cha <noum.rzdr@gmail.com>
2022-08-23 20:59:41 +0300

    message

--- src/App/Components/components/HistoryContainer.js
+++ src/App/Components/components/HistoryContainer.js
@@ -1,4 +1,4 @@
-import React , {useState} from "react"
+import React , {useState,useEffect} from "react"
 import axios from "axios"
 import Paper from "@mui/material/Paper";
 import Chip from "@mui/material/Chip"

7ec6761c8d6c39087a9ea9215d85e7871456fad8
Shiro-cha <noum.rzdr@gmail.com>
2022-08-24 22:47:27 +0300

    message

--- src/App/Components/explorer.js
+++ src/App/Components/explorer.js
@@ -265,7 +265,7 @@ export default function Explorer({remote,setPageComponent}) {
 		>
 		<DialogTitle>{`You must say something about your action`}</DialogTitle>
 		<DialogContent>
-		
+		 
 		<Input type="text" id="message" placeholder="Type a message" sx={{width:"100%",marginBottom:"30px"}} name="message" autofocus onChange={handleChangeMessage}/>
 		
 		</DialogContent>

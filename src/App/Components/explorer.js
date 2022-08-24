f0b79f07bcb7be36aeb88ae352d8a0e51f7f1a96
Shiro-cha <noum.rzdr@gmail.com>
2022-08-24 22:49:08 +0300

    message

--- src/App/Components/explorer.js
+++ src/App/Components/explorer.js
@@ -285,7 +285,7 @@ export default function Explorer({remote,setPageComponent}) {
 		open={whichAction}
 		message={actionName }
 		key={"top" + "center"}
-		sx={{opacity:"0.6"}}
+		sx={{opacity:"0.6",background:"#D51062"}}
 		/>
 		
 		</div>

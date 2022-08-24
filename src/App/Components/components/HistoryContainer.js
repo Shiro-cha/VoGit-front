e8bf81c31db66ef098db1cfc15022195c4274370
Shiro-cha <noum.rzdr@gmail.com>
2022-08-24 05:57:06 +0300

    message

--- src/App/Components/components/HistoryContainer.js
+++ src/App/Components/components/HistoryContainer.js
@@ -4,6 +4,7 @@ import Paper from "@mui/material/Paper";
 import Chip from "@mui/material/Chip"
 import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
+import Skeleton from "@mui/material/Skeleton"
 import Grid from '@mui/material/Grid';
 import IconButton from "@mui/material/IconButton"
 import Avatar from '@mui/material/Avatar'
@@ -65,54 +66,68 @@ export default function HistoryContainer(){
 	} 
 	
 	let ListeContainersDistant = ()=>{
+		if(containersDistant.length > 0){
 		
-		return(
-			 
-			containersDistant.map(function(cont){
+			return(
 				
-				return(
-					<>
-				<Grid item xs={11}>	
-				<ListItemButton onClick={()=>{cont.open = !cont.open}} className="w-100 border-1">
-				<Avatar sx={{backgroundColor:"#D51062"}}>
-				<Cyclone />
-				</Avatar>&nbsp;&nbsp;&nbsp;
-				<ListItemText primary="My message here" />
-				<Typography sx={{ fontSize:"12px",color:"#7d7d7d" ,display:"inline"}}>{cont.content.path}</Typography >
-				{open ? <ExpandLess /> : <ExpandMore />} 
-				</ListItemButton>
-				</Grid>
-				<Grid item xs={1}>
-				<IconButton><Delete /></IconButton>
-				</Grid>
-				<Collapse className="w-100" in={cont.open} timeout="auto" unmountOnExit>
-				<List className="w-100">
-				<ListItem
-				secondaryAction={
-					<IconButton edge="end" aria-label="action">
-					<MoreVert />
-					</IconButton>}
-					>
-					<ListItemAvatar>
-					<Avatar>
-					<Commit />
-					</Avatar>
-					</ListItemAvatar>
-					<ListItemText
-					primary="Thrid commit of VoGit"
+				containersDistant.map(function(cont){
 					
-					/>
-					<Typography sx={{ fontSize:"12px" ,color:"#7d7d7d",display:"inline"}}>777baf3cdc592803940b1aaeb72e166598821d0c</Typography >
-					</ListItem>
+					return(
+						<>
+						<Grid item xs={11}>	
+						<ListItemButton onClick={()=>{cont.open = !cont.open}} className="w-100 border-1">
+						<Avatar sx={{backgroundColor:"#D51062"}}>
+						<Cyclone />
+						</Avatar>&nbsp;&nbsp;&nbsp;
+						<ListItemText primary="My message here" />
+						<Typography sx={{ fontSize:"12px",color:"#7d7d7d" ,display:"inline"}}>{cont.content.path}</Typography >
+						{open ? <ExpandLess /> : <ExpandMore />} 
+						</ListItemButton>
+						</Grid>
+						<Grid item xs={1}>
+						<IconButton><Delete /></IconButton>
+						</Grid>
+						<Collapse className="w-100" in={cont.open} timeout="auto" unmountOnExit>
+						<List className="w-100">
+						<ListItem
+						secondaryAction={
+							<IconButton edge="end" aria-label="action">
+							<MoreVert />
+							</IconButton>}
+							>
+							<ListItemAvatar>
+							<Avatar>
+							<Commit />
+							</Avatar>
+							</ListItemAvatar>
+							<ListItemText
+							primary="Thrid commit of VoGit"
+							
+							/>
+							<Typography sx={{ fontSize:"12px" ,color:"#7d7d7d",display:"inline"}}>777baf3cdc592803940b1aaeb72e166598821d0c</Typography >
+							</ListItem>
+							
+							</List>
+							</Collapse>	
+							</>
+					)
+					
+				})
 				
-					</List>
-					</Collapse>	
-					</>
-							  )
-				
-			})
+			)
 			
-		)
+		}else{
+			return(
+				<>
+				<Skeleton width="100%" height="80px">
+				<Skeleton width="100%" height="80px">
+				<Skeleton width="100%" height="80px">
+				<Skeleton width="100%" height="80px">
+				</>
+			)
+		}
+		
+		
 		
 	}
 	let listeHistories = ()=>{

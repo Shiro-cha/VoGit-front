147ff16e731c9896b984d225ee9f68837c8b5c1f
Shiro-cha <noum.rzdr@gmail.com>
2022-08-24 06:16:42 +0300

    message

--- src/App/Components/components/HistoryContainer.js
+++ src/App/Components/components/HistoryContainer.js
@@ -91,7 +91,7 @@ export default function HistoryContainer(){
 						<Grid item xs={1}>
 						<IconButton><Delete /></IconButton>
 						</Grid>
-						<Collapse className="w-100" in={openListTemp[cont.open]} timeout="auto" unmountOnExit>
+						<Collapse className="w-100" in={openList[cont.open]} timeout="auto" unmountOnExit>
 						<List className="w-100">
 						<ListItem
 						secondaryAction={

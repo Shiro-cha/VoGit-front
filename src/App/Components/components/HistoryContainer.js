97087bdca56efede7bff7629e6e341bb6a8a8656
Shiro-cha <noum.rzdr@gmail.com>
2022-08-24 05:26:53 +0300

    message

--- src/App/Components/components/HistoryContainer.js
+++ src/App/Components/components/HistoryContainer.js
@@ -202,7 +202,7 @@ export default function HistoryContainer(){
 
 		<ListeContainersDistant/>
 		
-		<Typography component="h1" sx={{textAlign:"center",fontWeight:"bold",width:"100%",margin:"20px 30px"}} color="#4d4d4d">Distant container</Typography>
+		<Typography component="h1" sx={{textAlign:"center",fontWeight:"bold",width:"100%",margin:"20px 30px"}} color="#4d4d4d">Local container</Typography>
 		
 		<Grid item xs={11}>
 		

1b678999feab369d3118b797df6120246fc9e37c
Shiro-cha <noum.rzdr@gmail.com>
2022-08-27 18:07:10 +0300

    message

--- src/App/Components/components/MenuHistory.js
+++ src/App/Components/components/MenuHistory.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Menu, { MenuProps } from '@mui/material/Menu';

8fdbf6196a545ff76674577c789420026d45e7b6
Shiro-cha <noum.rzdr@gmail.com>
2022-08-27 14:35:00 +0300

    message

--- src/App/Components/components/HistoryContainer.js
+++ src/App/Components/components/HistoryContainer.js
@@ -264,7 +264,7 @@ export default function HistoryContainer(){
 				let containerTemp = []
 				let openListTemp = openList 
 				for (let i = 0 ; i < res.data["local"].length ; i++){
-					
+					console.log("iiiiii")
 					api.post("/svc/log/",{path:res.data["local"][i].path}).then(function(resc){
 						if(i===0){
 							containerTemp = []		

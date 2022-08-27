dfe96d069d2647085bebed0d2b36ace7dbba08b9
Shiro-cha <noum.rzdr@gmail.com>
2022-08-27 10:58:46 +0300

    message

--- src/App/config/baseURL.js
+++ src/App/config/baseURL.js
@@ -1,6 +1,6 @@
  
 const baseURL ={
-	baseURL:"http://ec2-54-149-158-31.us-west-2.compute.amazonaws.com:3001"  
+	baseURL:"54.149.158.31:3001"  
 // 	,headers:{"Access-Control-Allow-Origin": "*"}
 }
 export default baseURL  

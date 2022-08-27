920b95e759268985706d816fcf29a5b838c5d8d7
Shiro-cha <noum.rzdr@gmail.com>
2022-08-27 04:46:38 +0300

    message

--- src/App/Components/components/FileList.js
+++ src/App/Components/components/FileList.js
@@ -60,7 +60,7 @@ export default function FileList({currentFolder,setCurrentFolder,sep,host}){
 		
 	},[currentFolder])
 	useEffect(()=>{
-		openPath("d","","","",setCurrentFolder,host); 
+		openPath("d",currentFolder,"","",setCurrentFolder,host); 
 	},[host])
 	
 	
